f8a26619b38860dd74f61a07f210c3168cf0cf25
itsbelarusky <explosionartx@gmail.com>
2022-01-24 19:56:51 +0300

    new project

---
+++ JS/Script.js
@@ -0,0 +1,51 @@
+let number = 5
+
+number = 1000000
+console.log(number)
+
+const number2 = {
+    a: 10
+}
+number2.a = 50
+console.log(number2)
+
+const obj = {
+    name: "alex",
+    age: 30,
+    isMarried: false
+};
+
+obj.isMarried = true
+obj.age = 18
+
+
+console.log(obj)
+const arr = ["arrow.png", 10, {}, [], "Sara", "apple.jpg"];
+
+// alert("Evgen JS PRO")
+
+// // const warning = confirm("Are you here?")
+// // console.log(warning)
+// const answer = prompt("Your age?", "");
+// console.log(answer + 10)
+
+// const answers = [];
+// answers[0] = prompt("как ваше имя?", "")
+// answers[1] = prompt("как ваше фамилия?", "")
+// answers[2] = prompt("сколько вам лет?", "")
+// document.write(answers)
+
+// const url = "toys";
+// console.log(`https://someurl.com/${url}/baby/5`)
+//
+// const firstName = "Evgen";
+// console.log(`Привет ${firstName}!!!`)
+// let incr = 10,
+//     decr = 10;
+// incr++;
+// decr--;
+// console.log(decr)
+ const isChecked = false;
+ const isClose = false;
+
+ console.log(isChecked || isClose);
