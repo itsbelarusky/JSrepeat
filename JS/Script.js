35fb3a0034cb91318d5dd503363b38df9c498c08
Evgeniy <explosionartx@gmail.com>
2022-01-24 21:41:44 +0300

    add switch and if

--- JS/Script.js
+++ JS/Script.js
@@ -1,26 +1,26 @@
-let number = 5
-
-number = 1000000
-console.log(number)
-
-const number2 = {
-    a: 10
-}
-number2.a = 50
-console.log(number2)
-
-const obj = {
-    name: "alex",
-    age: 30,
-    isMarried: false
-};
-
-obj.isMarried = true
-obj.age = 18
-
-
-console.log(obj)
-const arr = ["arrow.png", 10, {}, [], "Sara", "apple.jpg"];
+// let number = 5
+//
+// number = 1000000
+// console.log(number)
+//
+// const number2 = {
+//     a: 10
+// }
+// number2.a = 50
+// console.log(number2)
+//
+// const obj = {
+//     name: "alex",
+//     age: 30,
+//     isMarried: false
+// };
+//
+// obj.isMarried = true
+// obj.age = 18
+//
+//
+// console.log(obj)
+// const arr = ["arrow.png", 10, {}, [], "Sara", "apple.jpg"];
 
 // alert("Evgen JS PRO")
 
@@ -52,30 +52,61 @@ const arr = ["arrow.png", 10, {}, [], "Sara", "apple.jpg"];
 
 //--------------Домашка1------------------
 
-const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
-console.log(numberOfFilms);
+// const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+// console.log(numberOfFilms);
+//
+//
+//
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
+// const a = prompt('Один из последних просмотренных фильмов?', "");
+// const b = prompt('На сколько оцените его?', "");
+// const c = prompt('Один из последних просмотренных фильмов?', "");
+// const d = prompt('На сколько оцените его?', "");
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
+//
+//
+// console.log(personalMovieDB)
+//
+// if (6 === 5) {
+//     console.log("Its true")
+// } else {
+//     console.log("Its false")
+// }
+
+
+// if (num < 3){
+//     console.log('False')
+// } else if (num < 5) {
+//     console.log("неправда")
+// } else {
+//     console.log("OK!")
+// }
+//
+// (num === 100) ? console.log("Good") : console.log("Bad")
+
+const num = 10;
+switch (num) {
+    case 40:
+        console.log("Ошибка");
+        break;
+    case 10:
+        console.log("В точку!!!");
+        break;
+    case 13:
+        console.log("Ошибка");
+        break;
+    default:
+        console.log("Не в этот раз!");
+        break;
+}
 
 
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
-const a = prompt('Один из последних просмотренных фильмов?', "");
-const b = prompt('На сколько оцените его?', "");
-const c = prompt('Один из последних просмотренных фильмов?', "");
-const d = prompt('На сколько оцените его?', "");
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
 
-
-console.log(personalMovieDB)
-
-
-
-
-
-//--------------Задание3------------------
