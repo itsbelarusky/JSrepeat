4350667192cbfc3166f6f1b0f7448283a75f6335
Evgeniy <explosionartx@gmail.com>
2022-04-27 20:26:09 +0300

    app movie repeat

--- JS/begin.js
+++ JS/begin.js
@@ -8,5 +8,23 @@
 // arrows[2] = prompt("как ваше жопа", "")
 //
 // document.write(arrows)
-const user = "lena";
-console.log(`привет ${user}`)
+// const user = "lena";
+// console.log(`привет ${user}`)
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?");
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+}
+const lastFilm = prompt("Один из последних просмотренных фильмов?", "");
+const answer = prompt("на сколько оцените его?", "" );
+const lastFilm2 = prompt("Один из последних просмотренных фильмов?", "");
+const answer2 = prompt("на сколько оцените его?", "");
+
+personalMovieDB.movies[lastFilm] = answer;
+personalMovieDB.movies[lastFilm2] = answer2;
+
+console.log(personalMovieDB)
