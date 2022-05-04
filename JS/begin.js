350d6c171df1a6d9c7300c7a93a08d8cc8b7db84
Evgeniy <explosionartx@gmail.com>
2022-05-04 22:44:45 +0300

    practic cicles

--- JS/begin.js
+++ JS/begin.js
@@ -10,22 +10,102 @@
 // document.write(arrows)
 // const user = "lena";
 // console.log(`привет ${user}`)
-const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?");
+// const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?");
+//
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// }
+// const lastFilm = prompt("Один из последних просмотренных фильмов?", "");
+// const answer = prompt("на сколько оцените его?", "" );
+// const lastFilm2 = prompt("Один из последних просмотренных фильмов?", "");
+// const answer2 = prompt("на сколько оцените его?", "");
+//
+// personalMovieDB.movies[lastFilm] = answer;
+// personalMovieDB.movies[lastFilm2] = answer2;
+//
+// console.log(personalMovieDB)
+// const newArr = [34, 434, 1]
+//
+// let num = 50;
+// while (num < 55) {
+//     console.log(num)
+//     num++
+//
+// }
+// do {
+// //     console.log(num)
+// //     num++
+// // } while (num < 55)
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
+// for (let i = 1; i < 8; i++) {
+//
+//     if (i === 6){
+//         continue;
+//     }
+//     console.log(i)
+// }
+
+// for (let i = 0; i < 3; i++) {
+//     console.log(i);
+//     for (let j = 0; j < 3; j++) {
+//         console.log(j);
+//     }
+// }
+//========================Задача на Елочку=============================
+// let result = "";
+// const length = 7;
+// for (let i = 1; i < length; i++) {
+//
+//     for (let j = 0; j < i; j++) {
+//         result += "*";
+//     }
+//
+//     result += '\n';
+// }
+//
+// console.log(result);
+//======================================================================
+
+// for (let i = 5; i <= 10; i++) {
+//     console.log(i)
+// }
+
+// for (let i = 20; i >=10 ; i--) {
+//     if (i === 13){
+//         break;
+//     }
+//     console.log(i);
+// }
+
+// for (let i = 2; i < 11; i++) {
+//     if (i % 2 === 0) {
+//         console.log(i);
+//     }
+// }
+
+// for (let i = 2; i <= 16; i++) {
+//     if (i % 2 === 0) {
+//         continue;
+//     } else {
+//         console.log(i);
+//     }
+// }
+
+// let num = 2;
+// while (num < 16) {
+//     num++
+//     if (num % 2 === 0) {
+//         continue;
+//     } else {
+//         console.log(num)
+//     }
+// }
+const arrayOfNumbers = [];
+for (let i = 5; i <= 10; i++) {
+    arrayOfNumbers[i - 5] = i;
 }
-const lastFilm = prompt("Один из последних просмотренных фильмов?", "");
-const answer = prompt("на сколько оцените его?", "" );
-const lastFilm2 = prompt("Один из последних просмотренных фильмов?", "");
-const answer2 = prompt("на сколько оцените его?", "");
-
-personalMovieDB.movies[lastFilm] = answer;
-personalMovieDB.movies[lastFilm2] = answer2;
-
-console.log(personalMovieDB)
-const newArr = [34, 434, 1]
+console.log(arrayOfNumbers);
